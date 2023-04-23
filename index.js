1637635875d45638db8ec26696283055d6db8c5b
creativestarjsp <creativestarjsp@gmail.com>
2023-04-23 21:48:45 +0530

    db

--- index.js
+++ index.js
@@ -149,14 +149,14 @@ PayCommission.belongsTo(Plot, { foreignKey: 'plot_id' });
 Payment.belongsTo(PlotBooking, { foreignKey: 'booking_id', as: 'plot_booking' });
 // Plot.belongsTo(Venture, { foreignKey: 'venture_id' });
 // Sync the database models
-sequelize.sync({alter:true,force:true})
+sequelize.sync({alter:false,force:false})
   .then(() => {
     console.log('Database connected and models synced.');
   })
   .catch((error) => {
     console.error('Unable to connect to the database:', error);
   });
-
+ 
 // Middleware for parsing JSON data in request bodies
 app.use(express.json());
 
