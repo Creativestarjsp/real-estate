9c2f398150840f7229829d4ab603bd8678624d3c
creativestarjsp <creativestarjsp@gmail.com>
2023-05-17 13:55:10 +0530

    17/05/23

--- Controller/user.js
+++ Controller/user.js
@@ -165,8 +165,8 @@ module.exports={
             'offer_price',
             'customer_id',
             [sequelize.literal('(offer_price )'), 'cost'],
-            [sequelize.fn('SUM', sequelize.col('payment.amount')), 'paid_amount'],
-            [sequelize.literal('(offer_price - SUM(payment.amount))'), 'balance'],
+            [sequelize.fn('SUM', sequelize.col('Payments.amount')), 'paid_amount'],
+            [sequelize.literal('(offer_price - SUM(Payments.amount))'), 'balance'],
           ],
           include: [
             {
